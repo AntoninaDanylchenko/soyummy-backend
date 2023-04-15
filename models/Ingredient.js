2c0b348fe9ea98c11998ab7bcdda5bd3c431faab
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-04-15 21:56:20 +0300

    add model ingredient

---
+++ models/Ingredient.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const ingredientSchema = new mongoose.Schema({
+  ttl: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  t: {
+    type: String,
+    required: true,
+  },
+  thb: {
+    type: String,
+    required: true,
+  },
+});
+
+const Ingredient = mongoose.model("Ingredient", ingredientSchema);
+
+module.exports = { Ingredient };
