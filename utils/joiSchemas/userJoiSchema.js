bd78d9cba0f666fd67d6a4739c9f9e4d931887d1
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-04-16 23:40:31 +0300

    add joiValidation for User-models

---
+++ utils/joiSchemas/userJoiSchema.js
@@ -0,0 +1,59 @@
+const Joi = require("joi");
+
+const { ObjectId } = require("mongoose").Types;
+
+const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,128}$/;
+
+const joiRegisterSchema = Joi.object({
+  username: Joi.string().min(2).max(32).required(),
+  password: Joi.string()
+    .pattern(passwordPattern)
+    .messages({
+      "string.pattern.base":
+        "Password should contain minimum eight characters, at least one letter and one number.",
+    })
+    .required(),
+  email: Joi.string().email().required(),
+  avatarURL: Joi.string().dataUri(),
+});
+
+const joiLoginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .pattern(passwordPattern)
+    .messages({
+      "string.pattern.base":
+        "Password should contain minimum eight characters, at least one letter and one number.",
+    })
+    .required(),
+});
+
+const joiFavoriteRecipes = Joi.object({
+  favoriteRecipes: Joi.array().items(Joi.string()),
+});
+
+const joiShoppingList = Joi.object({
+  shoppingList: Joi.array()
+    .items(
+      Joi.object({
+        ingredientId: Joi.string()
+          .custom(ObjectId.isValid, "Object Id Validation")
+          .required(),
+        measure: Joi.array().items(Joi.string()).default([]),
+      })
+    )
+    .default([]),
+});
+const joiOwnRecipes = Joi.object({
+  ownRecipes: Joi.array().items(
+    Joi.string().custom(ObjectId.isValid, "Object Id Validation")
+  ),
+});
+
+module.exports = {
+  joiRegisterSchema,
+  joiLoginSchema,
+  joiFavoriteRecipes,
+  joiShoppingList,
+  joiOwnRecipes,
+};
