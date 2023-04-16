2c35493f0656a4f127da0fbc2796e09e119238f1
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-04-16 15:27:48 +0300

    add ingredients list and ing-recipe

---
+++ controllers/ingredients.js
@@ -0,0 +1,36 @@
+const { Ingredient } = require("../models/Ingredient");
+const { Recipe } = require("../models/Recipe");
+
+const { wrapper } = require("../middlewares/wrapper");
+const { HttpError } = require("../utils/HttpError");
+
+let ingredientsList = async (req, res, next) => {
+  const ingredientList = await Ingredient.find();
+  if (!ingredientList) {
+    throw new HttpError(404, "Not found ingredients list");
+  }
+  res.status(200).json({
+    ingredientList,
+  });
+};
+
+ingredientsList = wrapper(ingredientsList);
+
+let ingredients = async (req, res, next) => {
+  const { query } = req.query;
+
+  const filterIngredient = await Ingredient.find({ ttl: { $eq: `${query}` } });
+
+  const result = await Recipe.find({
+    ingredients: {
+      $elemMatch: {
+        id: filterIngredient[0]._id,
+      },
+    },
+  });
+  res.status(200).json({ result });
+};
+
+ingredients = wrapper(ingredients);
+
+module.exports = { ingredientsList, ingredients };
