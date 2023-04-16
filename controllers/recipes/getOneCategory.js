351af655ccb93c776e13007a282ea656129f52c5
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-04-16 17:25:26 +0300

    add category eight orders list show

--- controllers/recipes/getOneCategory.js
+++ controllers/recipes/getOneCategory.js
@@ -1,1 +1,32 @@
-// const { wrapper } = require("../middlewares/wrapper");
+const { wrapper } = require("../../middlewares/wrapper");
+const { Recipe } = require("../../models/Recipe");
+const { HttpError } = require("../../utils/HttpError");
+
+let getOneCategory = async (req, res, next) => {
+  const { page, limit } = req.query;
+  const { category } = req.params;
+
+  const paginationPage = +page || 1;
+  const paginationLimit = +limit || 8;
+  const skip = (paginationPage - 1) * paginationLimit;
+
+  const recipesList = await Recipe.find({ category })
+    .skip(skip)
+    .limit(paginationLimit);
+
+  if (!recipesList) {
+    throw new HttpError(404, "Not recipes found");
+  }
+
+  const recipeCount = await Recipe.find({ category }).count();
+
+  if (!recipeCount) {
+    throw new HttpError(404, "Not recipes count found");
+  }
+
+  res.status(200).json({ total: recipeCount, recipesList });
+};
+
+getOneCategory = wrapper(getOneCategory);
+
+module.exports = { getOneCategory };
