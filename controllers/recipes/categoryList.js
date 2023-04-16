76ddf95d152ebf11c43c49fbe67a604392521d99
TosiaDan <antonina.ivanets@gmail.com>
2023-04-16 10:23:40 +0200

    adds categoryList

--- controllers/recipes/categoryList.js
+++ controllers/recipes/categoryList.js
@@ -1,14 +1,22 @@
 const { wrapper } = require("../../middlewares/wrapper");
 const { Recipe } = require("../../models/Recipe");
+const { HttpError } = require("../../utils/HttpError");
 
 let getCategoryList = async (req, res, next) => {
   const categories = await Recipe.find({}, { category: 1, _id: 0 }).sort({
     category: 1,
   });
-  const uniqueCategories = categories.map((category) =>
-    Object.values(category)
-  );
-  //   console.log(uniqueCategories);
+
+  if (!categories) {
+    throw new HttpError(404, "Not found");
+  }
+
+  const uniqueCategories = categories
+    .map((category) => {
+      return category.category;
+    })
+    .filter((category, index, array) => array.indexOf(category) === index);
+
   res.status(200).json(uniqueCategories);
 };
 
