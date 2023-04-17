b490eedf6985291c5e8f6daecbd1541e4de90580
ArtemKolesnyk <kolesnik2405@gmail.com>
2023-04-18 01:07:02 +0200

    test mailtrap.

--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -1,12 +1,21 @@
 const nodemailer = require("nodemailer");
 
-const emailTransport = nodemailer.createTransport({});
+const newUser = require("../controllers/auth");
+
+const emailTransport = nodemailer.createTransport({
+  host: process.env.EMAIL_MAILTRAP_HOST,
+  port: process.env.EMAIL_MAILTRAP_PORT,
+  auth: {
+    user: process.env.EMAIL_MAILTRAP_USERNAME,
+    pass: process.env.EMAIL_MAILTRAP_PASSWORD,
+  },
+});
 
 const emailConfig = {
   from: "So Yummy APP admin <soyummyadmin@op.pl>",
   to: newUser.email,
   subject: "Hello from So Yummy APP!",
-  text: "nice to meet you1",
+  text: "nice to meet you",
 };
 
-await emailTransport.sendMail(emailConfig);
+emailTransport.sendMail(emailConfig);
