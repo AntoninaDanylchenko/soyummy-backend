7a6cf8771da329b8611869b12db576ac7fe17672
Oksana-Pak <okspak7@gmail.com>
2023-04-17 16:01:46 +0300

    change some logic with wrapper function in recipes-main

--- routes/api/recipesRouter.js
+++ routes/api/recipesRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-const { wrapper } = require('../../middlewares/wrapper');
 const { getCategoryList } = require('../../controllers/recipes/categoryList');
 const { getOneCategory } = require('../../controllers/recipes/getOneCategory');
 const { getAllRecipes } = require('../../controllers/recipes/mainPage');
@@ -9,7 +8,7 @@ const router = express.Router();
 
 router.get('/category-list', getCategoryList);
 
-router.get('/main-page', wrapper(getAllRecipes));
+router.get('/main-page', getAllRecipes);
 
 router.get('/:category', getOneCategory);
 
