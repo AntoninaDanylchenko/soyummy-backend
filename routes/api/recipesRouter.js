ef9b2adfe59ed86b4594f292f7e5bba58ec2001c
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-04-17 18:04:30 +0300

    add recipe by id controller

--- routes/api/recipesRouter.js
+++ routes/api/recipesRouter.js
@@ -4,6 +4,7 @@ const { wrapper } = require('../../middlewares/wrapper');
 const { getCategoryList } = require('../../controllers/recipes/categoryList');
 const { getOneCategory } = require('../../controllers/recipes/getOneCategory');
 const { getAllRecipes } = require('../../controllers/recipes/mainPage');
+const { getRecipeById } = require('../../controllers/recipes/getReceptesById');
 
 const router = express.Router();
 
@@ -13,6 +14,6 @@ router.get('/main-page', wrapper(getAllRecipes));
 
 router.get('/:category', getOneCategory);
 
-// router.get("/:id");
+router.get('/id/:id', getRecipeById);
 
 module.exports = { recipesRouter: router };
