481f09926e180938dc651000f0d838c7094bdcad
Antonina Danylchenko <108192212+AntoninaDanylchenko@users.noreply.github.com>
2023-04-17 18:59:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const mongoose = require("mongoose");
 
 const {
   PORT = 3000,
-  DB_URL = "mongodb+srv://antoninaivanets:b4W2nVwP3QZbl1bU@cluster0.hb9jpwm.mongodb.net/db_soyummy?retryWrites=true&w=majority",
-} = process.env;
+  DB_URL } = process.env;
 
 (async () => {
   try {
