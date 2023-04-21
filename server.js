c0105fbc3e0e92ccf7cf3db1c7d81b6ef1e4b498
TosiaDan <antonina.ivanets@gmail.com>
2023-04-21 23:37:06 +0200

    correct

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ require("dotenv").config();
 const { app } = require("./app");
 const mongoose = require("mongoose");
 
-const { PORT = 3000, DB_URL } = process.env;
+const { DB_URL } = process.env;
 
+const port = process.env.PORT || 5000;
 (async () => {
   try {
     mongoose.set("strictQuery", true);
     await mongoose.connect(DB_URL);
     console.log("Success db connected");
-    app.listen(PORT, () => {
+    app.listen(port, () => {
       console.log("Server start successful");
     });
   } catch (error) {
