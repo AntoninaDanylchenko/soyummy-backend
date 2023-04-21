e3ed152a8a727734555d70d423180fbb61d8551f
TosiaDan <antonina.ivanets@gmail.com>
2023-04-21 23:29:48 +0200

    correct port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { app } = require("./app");
 const mongoose = require("mongoose");
 
-const { PORT = 5500, DB_URL } = process.env;
+const { PORT = 3000, DB_URL } = process.env;
 
 (async () => {
   try {
