7638b34ad87919b80db16a81d9230614f2471f26
ArtemKolesnyk <kolesnik2405@gmail.com>
2023-04-24 23:50:03 +0200

    fix sucscribe

--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -3,29 +3,21 @@ const path = require("path");
 const { convert } = require("html-to-text");
 const pug = require("pug");
 
-const { subscribeSchema } = require("../utils/joiSchemas/subscribeSchema");
-const { HttpError } = require("../utils/HttpError");
-
 const { SENDGRID_API_KEY, SENGRID_EMAIL_FROM } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const subsribeMail = async (req, res) => {
-  const { error } = subscribeSchema.validate(req.body);
-  if (error) {
+  const { email } = req.body;
 
-    throw new HttpError(error.status, error.message);
-
-  }
   const html = pug.renderFile(
     path.join(__dirname, "..", "/", "template", "tplMail.pug")
   );
-  const email = req.body.email;
 
   const emailTpl = {
     to: email,
     from: SENGRID_EMAIL_FROM,
-    subject: "Subscription info",
+    subject: "Thank you for subscribe!",
     html: html,
     text: convert(html),
   };
@@ -35,6 +27,8 @@ const subsribeMail = async (req, res) => {
     .catch((error) => {
       console.log(error.message);
     });
-  res.status(200).json("Email  is send");
+  res.status(200).json({
+    message: "A message has been sent to this email. Check your mail.",
+  });
 };
 module.exports = { subsribeMail };
