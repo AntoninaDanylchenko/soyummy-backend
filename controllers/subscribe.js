2c53902113d36e1a70457821ef2d7a09e85336a5
ArtemKolesnyk <kolesnik2405@gmail.com>
2023-05-01 14:46:47 +0200

    Update subscribe.js

--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -1,7 +1,7 @@
 const sgMail = require("@sendgrid/mail");
-const path = require("path");
-const { convert } = require("html-to-text");
-const pug = require("pug");
+// const path = require("path");
+// const { convert } = require("html-to-text");
+// const pug = require("pug");
 
 const { SENDGRID_API_KEY, SENGRID_EMAIL_FROM } = process.env;
 
@@ -10,29 +10,24 @@ sgMail.setApiKey(SENDGRID_API_KEY);
 const subsribeMail = async (req, res) => {
   const { email } = req.body;
 
-  const html = pug.renderFile(
-    path.join(__dirname, "..", "/", "template", "tplMail.pug")
-  );
-
-  console.log(email);
+  // const html = pug.renderFile(
+  //   path.join(__dirname, "..", "/", "template", "tplMail.pug")
+  // );
 
   const emailTpl = {
     to: email,
     from: SENGRID_EMAIL_FROM,
-    cc: "antonina.ivanets@ukr.net",
     subject:
       "Thank you for your subscription to the news So Yummy. Be the first to receive the newest recipes!",
-    html: html,
-    text: convert(html),
+    html: "<strong>test memail sandgrid</strong>",
+    text: "<strong>test memail sandgrid</strong>",
   };
-
   await sgMail
     .send(emailTpl)
     .then(() => console.log("send email success"))
     .catch((error) => {
       console.log(error.message);
     });
-
   res.status(200).json({
     message: "A message has been sent to this email. Check your mail.",
   });
